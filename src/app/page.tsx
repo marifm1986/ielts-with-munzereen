57bd45db3eb870154592b2b33156c7676d75cc80
arif matubber <am@hoxro.com>
2025-07-26 16:28:40 +0600

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default async function Product() {
 
   const productData = await res.json();
   const typedProductData = productData.data as Data;
-
+  console.log(typedProductData)
   const heroStyle = {
     backgroundImage: `url("https://cdn.10minuteschool.com/images/ui_%281%29_1716445506383.jpeg")`,
     width: '100vw',
@@ -84,54 +84,51 @@ export default async function Product() {
   const courseDetails = typedProductData.sections.find((x: any) => x.type === "about");
 
   return (
-    <main className="flex flex-col bg-amber-50 h-lvh">
-      <div className="hero-section" style={heroStyle}>
-        <div className="container mx-auto flex flex-col gap-2">
-          <h1 className="text-white text-[21px] font-semibold md:text-4xl">{typedProductData.title}</h1>
-          <a href="#" className='flex gap-1.5'>
-            <Image src="https://cdn.10minuteschool.com/images/Dev_Handoff_Q1_24_Frame_2_1725444418666.png" alt="Rating badge" width={100} height={24} />
-            (82.6% শিক্ষার্থী কোর্স শেষে ৫ রেটিং দিয়েছেন)
-          </a>
-          <p className="text-gray-400" dangerouslySetInnerHTML={{ __html: typedProductData.description }} />
-        </div>
-      </div>
-
-      <div className="container flex">
-        <div className="flex-2 md:max-w-[calc(100%_-_348px)] lg:max-w-[calc(100%_-_448px)]">
+    <main className="flex  bg-gray-100 h-lvh">
+      <div className="container flex text-black mx-auto p-6 gap-8">
+        <div className="main-content flex-2/3 flex flex-col gap-8">
+          <div className="hero-section flex flex-col gap-4">
+            <h1 className="text-[21px] font-semibold md:text-4xl">{typedProductData.title}</h1>
+            <a href="#" className='flex gap-1.5'>
+              <Image src="https://cdn.10minuteschool.com/images/Dev_Handoff_Q1_24_Frame_2_1725444418666.png" alt="Rating badge" width={100} height={100} />
+              (82.6% শিক্ষার্থী কোর্স শেষে ৫ রেটিং দিয়েছেন)
+            </a>
+            <p className="" dangerouslySetInnerHTML={{ __html: typedProductData.description }} />
+          </div>
           {/* Instructor */}
           {instructor && (
-            <>
+            <div className='instructor-wrapper flex flex-col gap-1 w-full'>
               <span className="mb-4 text-xl font-semibold md:text-2xl">Course instructor</span>
-              <section className='instructor-card'>
-                <div className="image-wrapper">
-                  <Image src={instructor.values[0]?.image} alt={instructor.values[0]?.name || "Instructor"} width={100} height={100} />
+              <section className='instructor-card flex gap-4 items-center w-full bg-gray-200 p-4 rounded-md overflow-hidden'>
+                <div className="image-wrapper overflow-hidden">
+                  <Image src={instructor.values[0]?.image} alt={instructor.values[0]?.name || "Instructor"} width={80} height={100} />
                 </div>
-                <div className="info-wrapper">
+                <div className="info-wrapper w-full">
                   <a className="name" href='#'>{instructor.values[0]?.name}</a>
-                  <p className="text-gray-400" dangerouslySetInnerHTML={{ __html: instructor.values[0]?.description }} />
+                  <p className="flex w-full" dangerouslySetInnerHTML={{ __html: instructor.values[0]?.description }} />
                 </div>
               </section>
-            </>
+            </div>
           )}
 
           {/* Features */}
           {features && (
-            <>
+            <div className='features-wrapper flex flex-col bg-white border-2 border-gray-200 gap-4 p-6 rounded-xl'>
               <span className="mb-4 text-xl font-semibold md:text-2xl">{features.name}</span>
-              <section className='feature-wrapper'>
+              <section className='feature-wrapper grid grid-cols-2 gap-4'>
                 {features.values.map((x: any) => (
-                  <div className="single-feature" key={x.id}>
-                    <span className="icon-wrapper">
-                      <Image src={x.icon} alt="Feature icon" width={32} height={32} />
-                    </span>
+                  <div className="single-feature gap-4 flex" key={x.id}>
+                    <div className="icon-wrapper">
+                      <Image className='object-contain object-center min-w-8' src={x.icon} alt="Feature icon" width={24} height={100} />
+                    </div>
                     <div className="info-wrapper">
-                      <span className="title">{x.title}</span>
+                      <span className="title text-xl font-bold">{x.title}</span>
                       <p>{x.subtitle}</p>
                     </div>
                   </div>
                 ))}
               </section>
-            </>
+            </div>
           )}
 
           {/* Pointers */}
@@ -181,31 +178,44 @@ export default async function Product() {
                 {courseDetails.values.map((x: any) => (
                   <div className="single-feature" key={x.id}>
                     <span dangerouslySetInnerHTML={{ __html: x.title }} />
-                    <p className="text-gray-400" dangerouslySetInnerHTML={{ __html: x.description }} />
+                    <p className="" dangerouslySetInnerHTML={{ __html: x.description }} />
                   </div>
                 ))}
               </section>
             </>
           )}
         </div>
-
         {/* Sidebar */}
-        <div className="sidebar flex flex-col flex-1">
-          <div className="video-trailer">
+        <div className="sidebar flex flex-col flex-1/3 ">
+          <div className="video-trailer flex flex-col shadow-2xl rounded-2xl overflow-clip gap-4">
             <Image
+              className='w-full'
               src={typedProductData.media[0]?.thumbnail_url || "/fallback.jpg"}
               alt="Video thumbnail"
               width={400}
               height={250}
             />
+            <div className="image-gallery flex gap-4 items-center overflow-auto">
+              {
+                typedProductData.media.map((x, id) => (
+                  x.thumbnail_url ? (
+                    <Image className='flex-1' src={x?.thumbnail_url} alt={x.thumbnail_url} width={100} height={100} key={id} />
+                  ) : null
+
+
+                ))
+              }
+            </div>
+            <span className="price text-3xl font-bold p-4">৳1,000</span>
+            <button className="cta bg-green-600 text-white p-4">{typedProductData.cta_text?.name}</button>
           </div>
-          <button className="cta">{typedProductData.cta_text?.name}</button>
           <span className="title">এই কোর্সে যা থাকছে</span>
           <div className="check-list flex flex-col gap-2">
             {typedProductData.checklist.map((check, index) =>
               check.list_page_visibility ? (
                 <div className="single-check flex gap-2" key={check.id || index} style={{ color: check.color }}>
                   <Image src={check.icon} alt={check.text} width={24} height={24} />
+
                   <span className="text">{check.text}</span>
                 </div>
               ) : null
@@ -213,6 +223,7 @@ export default async function Product() {
           </div>
         </div>
       </div>
+
     </main>
   );
 }
